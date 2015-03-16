309b83461dfbbb5ab8a9b1711073da14522c645d
dantangfan <dantangfan@gmail.com>
2015-03-16 10:52:43 +0800

    fix bug

--- restart.sh
+++ restart.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 sudo kill `ps -aux | grep worker.py | awk '{print $2}'`
-for((i=0;i<4;i++))
-do
-    nohup python debug.py runserver --host 0.0.0.0 &
-done
+
+nohup python debug.py runserver --host 0.0.0.0 &
